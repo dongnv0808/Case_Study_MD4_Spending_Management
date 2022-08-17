c949200b11f687138c89113b456223c92853fd34
giabao2k <ngonguyengiabao2000@gmail.com>
2022-08-17 15:11:24 +0700

    add list category be fe

--- server.ts
+++ server.ts
@@ -2,7 +2,8 @@ import bodyParser from "body-parser";
 import express from "express";
 import fileUpload from "express-fileupload";
 import { routes } from "./src/routes/routes";
-import dataSource from "src/data-source";
+import dataSource from "./src/data-source";
+import cors from 'cors';
 
 let PORT = 3000;
 const app = express();
@@ -10,7 +11,8 @@ dataSource.connect();
 app.use(bodyParser.json());
 app.use(express.json());
 app.set('views', './src/views');
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(cors());
 app.use(fileUpload({
     createParentPath: true
 }))
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,3 +1,4 @@
 import { Router } from "express";
-
+import { categoryRouter } from "./category-route";
 export const routes = Router();
+routes.use('/categories',categoryRouter)
